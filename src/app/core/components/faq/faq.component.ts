e5c1ce2426b9da88563695900acfd6b1674bc645
Patricio Abarca <patricioabarca88@gmail.com>
2026-02-07 21:03:48 -0300

    se agregan imagenes

--- src/app/core/components/faq/faq.component.ts
+++ src/app/core/components/faq/faq.component.ts
@@ -16,22 +16,51 @@ export class FaqComponent {
     faqs: FaqItem[] = [
         {
             question: '¿Qué tecnologías usas?',
-            answer: 'Uso Angular para el frontend, Spring Boot para el backend y Flutter para desarrollo móvil multiplataforma.',
+            answer: `Utilizo Angular para el desarrollo del frontend web, Spring Boot para el backend y Flutter para aplicaciones móviles multiplataforma. Además, trabajo con herramientas como Firebase para bases de datos móviles y notificaciones, PostgreSQL para almacenamiento de datos, y Photoshop, Illustrator y Figma para diseño de interfaces y experiencias de usuario.
+`,
             open: false,
         },
         {
-            question: '¿Cuales son los precios?',
-            answer: 'Sí, trabajo como desarrollador freelance.',
+            question: '¿Cuáles son los precios?',
+            answer: 'No hay un precio fijo por servicio, depende de la complejidad del proyecto. Este se determina con un presupuesto que se le envia al cliente y se basa en la guía de precios que se encuentra en el documento <a href="ruta/del/documento.pdf" target="_blank" rel="noopener noreferrer">"Servicios y condiciones"</a> donde se establecen guías de cobro dependiendo de varios parametros relevantes que determinan el precio. Por ejemplo, cuantas secciones tiene la web, funcionalidades, diseño, formularios, etc. Pero, puedes ver esa guía y hacer un estimado de acuedo a lo que necesitas y en que plan entraría tu solicitud.',
+            open: false,
+        },
+
+        {
+            question:
+                '¿Emites factura por tus servicios de diseño web y desarrollo?',
+            answer: 'Sí, entrego factura electrónica por todos mis servicios, asegurando respaldo legal y formalidad.',
             open: false,
         },
         {
-            question: '¿Realizas mantenimiento a las webs?',
-            answer: 'Sí, trabajo como desarrollador freelance.',
+            question:
+                '¿Qué tipo de soporte ofreces después de entregar el proyecto?',
+            answer: 'Una vez terminado el trabajo ofrezco soporte básico y mantención gratis durante 1 mes con las condiciones especificadas en el documento <a href="ruta/del/documento.pdf" target="_blank" rel="noopener noreferrer">"Servicios y condiciones"</a>. El soporte básico gratis del primer mes cubre únicamente corrección de errores y ajustes menores. Cualquier funcionalidad o cambio adicional fuera de estas condiciones se presupuestará aparte según la guía precios en el apartado de Servicio Técnico.',
+
+            // El primer mes de mantención es gratis con las condiciones especificadas en el documento <a href="ruta/del/documento.pdf" target="_blank" rel="noopener noreferrer">"Servicios y condiciones"</a>.',
+            open: false,
+        },
+
+        {
+            question: '¿Los diseños que muestras son los que debo escoger?',
+            answer: 'No, esos diseños y algunos proyectos son ejemplos de lo que puedo hacer y de mi estilo, tu sitio o aplicación sera como tu consideres (tu diseño) o contratar el servicio de diseño más el desarrollo que tienen un precio como pack.',
+            open: false,
+        },
+
+        {
+            question: '¿Haces soporte a proyectos NO realizado por ti?',
+            answer: 'Los precios de mantenimiento especificados en el documento <a href="ruta/del/documento.pdf" target="_blank" rel="noopener noreferrer">"Servicios y condiciones"</a> son para proyectos realizados por mi. Sin embargo, existe la posiblidad de crear un presupuesto con la guia de precios de Servicio Técnico a proyectos heredados (no creados por mi) dependiendo de la complejidad del código heredado, lenguaje de programación, técnologias, etc.',
+            open: false,
+        },
+
+        {
+            question: '¿Es útil la guía de precios?',
+            answer: 'La guía es una referencia relativa y general de precios. Los proyectos se clasifican agrupando diferentes especificaciones y caracteristicas según mí criterio. Un proyecto, podría entrar en la categoría de plan básico y se cobrara así y así sucesivamente. Tambien, detallo técnicamente al cliente porque entraría un proyecto en el plan X, Y, Z.',
             open: false,
         },
         {
             question: '¿Tienes la agenda disponible?',
-            answer: 'Sí, trabajo como desarrollador freelance.',
+            answer: 'Sí, actualmente tengo agenda disponible.',
             open: false,
         },
     ];
